49cdc51a9a8504da243ba167ef48a1b3c5c3be66
Roger0432 <122436740+Roger0432@users.noreply.github.com>
2026-03-23 19:09:30 +0100

    styles

--- beerunners/src/App.tsx
+++ beerunners/src/App.tsx
@@ -1,74 +1,98 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
     const [cerveses , setCerveses] = useState(0);
     const [kms, setKms] = useState(0);
 
-    useEffect(() => {
-        setCerveses(0);
-    }, []);
-
     return (
         <>
-            <div className="flex justify-center items-center">
+            <div className="min-h-screen bg-gray-900 flex justify-center items-start py-8 px-4 sm:px-6 lg:px-8">
 
-                <div className="flex flex-col max-w-150 w-full gap-5">
+                <div className="flex flex-col w-full max-w-4xl gap-5">
                     
-                    <div className="text-center text-xl text-gray-500 font-bold rounded p-5 bg-gray-200 w-105 mx-auto mt-5">
+                    <div className="text-center text-2xl sm:text-3xl text-gray-100 font-bold rounded p-5 bg-gray-800 w-full mx-auto">
                         🍺 BEERUNNERS 🏃
                     </div>
 
                     {/* comptadors */}
-                    <div className="flex gap-5 justify-center">
+                    <div className="flex flex-col sm:flex-row gap-4 justifiy-center">
                         
                         {/* comptador de cerveses */}
-                        <div className="text-center flex flex-col rounded p-5 bg-yellow-100 w-50">
-                            <div className="text-yellow-500 font-bold">
-                                <div>Cerveses</div>
-                                <div>{cerveses}</div>
-                            </div>
+                        <div className="text-center flex flex-col rounded p-5 bg-yellow-100 w-full sm:w-1/2">
+                            <div className="text-yellow-500 font-bold text-3xl sm:text-4xl">{cerveses}</div>
+                            <div className="text-gray-500">Cerveses</div>
                         </div>
 
                         {/* comptador de kms */}
-                        <div className="text-center flex flex-col rounded p-5 bg-blue-100 w-50">
-                            <div className="text-blue-500 font-bold">
-                                <div>Quilòmetres</div>
-                                <div>{kms}</div>
-                            </div>
+                        <div className="text-center flex flex-col rounded p-5 bg-blue-100 w-full sm:w-1/2">
+                            <div className="text-blue-500 font-bold text-3xl sm:text-4xl">{kms}</div>
+                            <div className="text-gray-500">Quilòmetres</div>
                         </div>
                     </div>
 
                     {/* diferència entre cerveses i kms */}
-                    <div className="text-center rounded p-5 bg-red-100 w-105 mx-auto">
-                        <div className="text-red-500 font-bold">
-                            <div>Diferència</div>
-                            <div>{kms - cerveses}</div>
+                    <div className="text-center rounded p-5 bg-red-100 w-full">
+                        <div className="text-3xl sm:text-4xl text-red-500 font-bold">
+                            {kms - cerveses}
+                        </div>
+                        <div className="text-gray-500">
+                            {kms - cerveses > 0 ? (
+                                    <div>🍻 Has de beure {Math.abs(kms - cerveses)} cerveses</div>
+                                ) : kms - cerveses < 0 ? (
+                                    <div>🥵 Has de correr {Math.abs(kms - cerveses)} kms</div>
+                                ) : (
+                                    <div>⚖️ Equilibri perfecte </div>
+                                )}
                         </div>
                     </div>
 
-                    {/* botó de sumar cerveses */}
-                    <div className="text-center">
-                        <button className="cursor-pointer rounded p-5 bg-yellow-100 text-yellow-500 font-bold w-50" onClick={() => setCerveses(cerveses + 1)}>
-                            Sumar Cerveses
-                        </button>
-                        🍺
-                        <button className="cursor-pointer rounded p-5 bg-yellow-100 text-yellow-500 font-bold w-50" onClick={() => setCerveses(cerveses - 1)}>
-                            Restar Cerveses
-                        </button>
+                    {/* comtadors */}
+                    <div className="flex flex-col sm:flex-row gap-4 justifiy-center">
+
+                        <div className="flex justify-center items-center rounded p-5 bg-yellow-100 w-full sm:w-1/2 cursor-pointer">
+                            <div className="text-4xl">🍺</div>
+                            <div className="text-2xl text-gray-500 font-bold">Cervesa</div>
+                        </div>
+
+                        <div className="flex justify-center items-center rounded p-5 bg-blue-100 w-full sm:w-1/2 cursor-pointer">
+                            <div className="text-4xl">🏃</div>
+                            <div className="text-2xl text-gray-500 font-bold">Córrer</div>
+                        </div>
+
                     </div>
 
-                    {/* botó de sumar kms */}
-                    <div className="text-center">
-                        <button className="cursor-pointer rounded p-5 bg-blue-100 text-blue-500 font-bold w-50" onClick={() => setKms(kms + 1)}>
-                            Sumar Kms
-                        </button>
-                        🏃
-                        <button className="cursor-pointer rounded p-5 bg-blue-100 text-blue-500 font-bold w-50" onClick={() => setKms(kms - 1)}>
-                            Restar Kms
-                        </button>
+
+
+
+                    {/*
+                     botó de sumar cerveses 
+                    <div className="text-center w-full">
+                        <div className="flex flex-col sm:flex-row items-center justify-center gap-2 w-full">
+                            <button className="cursor-pointer rounded px-4 py-2 bg-yellow-100 text-yellow-500 font-bold w-full sm:w-44" onClick={() => setCerveses(cerveses + 1)}>
+                                Sumar Cerveses
+                            </button>
+                            <span className="text-xl">🍺</span>
+                            <button className="cursor-pointer rounded px-4 py-2 bg-yellow-100 text-yellow-500 font-bold w-full sm:w-44" onClick={() => setCerveses(cerveses - 1)}>
+                                Restar Cerveses
+                            </button>
+                        </div>
                     </div>
+
+                    botó de sumar kms
+                    <div className="text-center w-full">
+                        <div className="flex flex-col sm:flex-row items-center justify-center gap-2 w-full">
+                            <button className="cursor-pointer rounded px-4 py-2 bg-blue-100 text-blue-500 font-bold w-full sm:w-44" onClick={() => setKms(kms + 1)}>
+                                Sumar Kms
+                            </button>
+                            <span className="text-xl">🏃</span>
+                            <button className="cursor-pointer rounded px-4 py-2 bg-blue-100 text-blue-500 font-bold w-full sm:w-44" onClick={() => setKms(kms - 1)}>
+                                Restar Kms
+                            </button>
+                        </div>
+                    </div>
+                    */}
 
                 </div>
 
